8aeb4326a646ceed27efc6e5ac60fff7ed9b256e
shrey17501 <90690884+shrey17501@users.noreply.github.com>
2023-12-12 11:58:55 +0530

    Update settings.py

--- allstores/settings.py
+++ allstores/settings.py
@@ -81,17 +81,17 @@ WSGI_APPLICATION = 'allstores.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'electronicstores',
-        'USER': 'postgres',
-        'PASSWORD': '12',
-        'HOST': 'localhost'
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'electronicstores',
+    #     'USER': 'postgres',
+    #     'PASSWORD': '12',
+    #     'HOST': 'localhost'
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
