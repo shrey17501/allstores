24d5a406823a5ced4660962999fec1f0e20da211
shrey17501 <90690884+shrey17501@users.noreply.github.com>
2023-12-18 18:07:27 +0530

    Update urls.py

--- electronics/urls.py
+++ electronics/urls.py
@@ -10,9 +10,5 @@ urlpatterns = [
     path("home/", views.home, name="home"),
     path("", views.loginUser, name="login"),
     path("signup", views.signup, name="signup"),
-    path('logout',views.logoutUser, name="logout"),
-    path('transferdata', views.transferdata, name='transferdata'), 
-    path('citystatefunction', views.citystatefunction, name='citystatefunction'), 
-    path('servicesfunction', views.servicesfunction, name='servicesfunction'),
-    path('canonfunction', views.canonfunction, name='canonfunction'),
+    path('logout',views.logoutUser, name="logout")
 ]
